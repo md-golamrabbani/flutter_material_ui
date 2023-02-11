92164284667eb995040ad9433b34f54e9eea8a35
Ashique Imran <imranashik50@gmail.com>
2023-02-11 18:57:43 +0600

    add gradinet button

--- lib/src/mbutton.dart
+++ lib/src/mbutton.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_material_ui/src/utils/app_constants.dart';
 
@@ -38,6 +40,41 @@ class MButton {
           alignment: isFullWidth == true ? Alignment.center : null,
           child: child,
         ),
+      ),
+    );
+  }
+
+  static Widget gradientBtn({
+    List<Color>? gradientColors,
+    double? borderRadius,
+    EdgeInsets? padding,
+    EdgeInsets? margin,
+    bool? isFullWidth,
+    required Widget child,
+    required VoidCallback onPressed,
+  }) {
+    return Container(
+      margin: margin ?? EdgeInsets.all(5.0),
+      padding: padding ?? EdgeInsets.all(5.0),
+      decoration: BoxDecoration(
+        gradient: gradientColors == null
+            ? null
+            : LinearGradient(
+                colors: gradientColors,
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+              ),
+        borderRadius: BorderRadius.all(
+          Radius.circular(borderRadius ?? AppConstants.defaultSize / 4),
+        ),
+      ),
+      child: GestureDetector(
+        onTap: onPressed,
+        child: MouseRegion(
+            cursor: SystemMouseCursors.click,
+            child: Container(
+                alignment: isFullWidth == true ? Alignment.center : null,
+                child: child)),
       ),
     );
   }
